98aa80c6e7e62a16b7db49cdbdae785251e30f7b
Kim Sanghae <pelikan@pusan.ac.kr>
2025-10-15 17:21:27 +0900

    refactor: 입출력 제외 다른 로직 유틸리티 클래스로 분리 사용
    - 파싱, 검증, 변환, 계산을 각각 유틸리티 클래스로 분리하여 사용하도록 변경

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,81 +1,23 @@
 package calculator;
 
-import static java.lang.Character.isDigit;
-
 import camp.nextstep.edu.missionutils.Console;
-import java.util.Arrays;
-import java.util.regex.Pattern;
 
 public class Application {
 
-    final static String CUSTOM_START = "//";
-    final static String CUSTOM_END = "\\n";
-    final static String FIRST_DELI = ",";
-    final static String SECOND_DELI = ":";
-    final static String SPACE = " ";
+    private final static String INPUT_DIALOGUE = "덧셈할 문자열을 입력해 주세요.";
+    private final static String OUTPUT_DIALOGUE = "결과 : ";
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        System.out.println(INPUT_DIALOGUE);
         var input = Console.readLine();
-        var customDeli = getCustomDeli(input);
-        String trimmed = input.substring(0, input.indexOf(CUSTOM_START));
-        trimmed += input.substring(input.indexOf(CUSTOM_END) + CUSTOM_END.length());
-        String[] delimieters = {FIRST_DELI, SECOND_DELI, customDeli};
-        checkInputString(trimmed, delimieters);
-        System.out.println("결과 : " + getResult(trimmed, customDeli));
-    }
-
-    private static void checkInputString(String trimmed, String[] delimieters) {
-        for (int i = 0; i < trimmed.length(); i++) {
-            if (isNotNumberOrDelimiters(trimmed, delimieters, i)) {
-                throw new IllegalArgumentException("입력은 구분자와 양수로 구성된 문자열이어야 합니다.");
-            }
+        if (input == null || input.isEmpty()) {
+            System.out.println(OUTPUT_DIALOGUE + 0);
+            return;
         }
+        var parsedStringExpressions = Parser.parse(input);
+        var values = Converter.convertStringToInteger(parsedStringExpressions);
+        Validator.validateValues(values);
+        System.out.println(OUTPUT_DIALOGUE + SumCalculator.getSum(values));
     }
 
-    private static boolean isNotNumberOrDelimiters(String trimmed, String[] delimieters, int i) {
-        return !isDigit(trimmed.charAt(i)) && !Arrays.asList(delimieters)
-            .contains(Character.toString(trimmed.charAt(i)));
-    }
-
-    private static String getCustomDeli(String input) {
-        String customDeli = "";
-        if (input.contains(CUSTOM_START) && input.contains(CUSTOM_END)) {
-            var startPos = input.indexOf(CUSTOM_START);
-            var endPos = input.indexOf(CUSTOM_END);
-            customDeli = input.substring(startPos + CUSTOM_START.length(), endPos);
-        }
-        return customDeli;
-    }
-
-    private static int getResult(String input, String customDeli) {
-        int result = 0;
-        if (hasDelimeters(input, customDeli)) {
-            String[] delimieters = {FIRST_DELI, SECOND_DELI, customDeli};
-            var regex = getStringBuilder(delimieters).toString();
-            var split = input.split(regex);
-            return getIntegerSum(split, delimieters, result);
-        }
-        return Integer.parseInt(input);
-    }
-
-    private static StringBuilder getStringBuilder(String[] delis) {
-        StringBuilder r = new StringBuilder("([");
-        Arrays.stream(delis).forEach(d -> r.append(Pattern.quote(d)));
-        r.append("])");
-        return r;
-    }
-
-    private static boolean hasDelimeters(String input, String customDeli) {
-        return input.contains(FIRST_DELI) || input.contains(SECOND_DELI) || (
-            !customDeli.isEmpty() && input.contains(customDeli));
-    }
-
-    private static int getIntegerSum(String[] split, String[] delis, int result) {
-        result += Arrays.stream(split)
-            .filter(s -> !Arrays.asList(delis).contains(s) && !s.isEmpty())
-            .mapToInt(Integer::parseInt).sum();
-        return result;
-    }
 }
